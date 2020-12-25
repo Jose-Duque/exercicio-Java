eb66787be17c93526af257f5835493ce88c3b321
José Carlos Duque Filho <cj_duque@hotmail.com>
2020-12-25 11:41:09 -0300

    Exercicio criado

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+
+/*String nome = "Maria";
+int idade = 31;
+double renda = 4000.0;
+System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);*/
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		//sc.nextLine();
+		//int x  = sc.nextInt();
+		
+		
+			
+		System.out.println();
+		
+	
+		sc.close();
+	}
+	
+}
+		
+		
+		
